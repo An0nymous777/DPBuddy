31dc0484dbeec958e6a68da9fc79e06259530cc1
PhysicsBuddy <140951700+PhysicsBuddy@users.noreply.github.com>
2023-11-18 09:44:43 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -22,3 +22,31 @@ function opentab(tabname) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+
+function resetForm() {
+    // Get the first form with the name
+    // Usually the form name is not repeated
+    // but duplicate names are possible in HTML
+    // Therefore to work around the issue, enforce the correct index
+    var title = document.getElementById('clastifyTitle');
+    var consection = document.getElementById('contact-us');
+    var thankssection = document.getElementById('thankyouSection');
+    consection.style.display = "none";
+    thankssection.style.display = "block";
+ }
+ 
+function submitForm(){
+    var frm = document.getElementById('clastify');
+    frm.submit(); // Submit the form
+    frm.reset();  // Reset all form data
+    return false; // Prevent page refresh
+}
+
+function goback() {
+    var title = document.getElementById('clastifyTitle');
+    var consection = document.getElementById('contact-us');
+    var thankssection = document.getElementById('thankyouSection');
+    consection.style.display = "block";
+    thankssection.style.display = "none";
+}
