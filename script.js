7761fabe25420a6147e9e99af4d7f22dae9b2bc1
PhysicsBuddy <140951700+PhysicsBuddy@users.noreply.github.com>
2023-12-12 06:11:15 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -50,3 +50,28 @@ function goback() {
     consection.style.display = "block";
     thankssection.style.display = "none";
 }
+
+
+// ----------annoucement--------
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function closeModal(modal){
+    if (modal==null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
